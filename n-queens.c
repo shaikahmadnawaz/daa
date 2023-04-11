e62f2f37fe7764375c3c161aa39e95523763921d
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-04-12 01:41:59 +0530

    n-queens.c

--- n-queens.c
+++ n-queens.c
@@ -97,3 +97,73 @@ int main()
 
   return 0;
 }
+
+#include <stdio.h>
+#define N 8
+
+int board[N][N];
+
+// Function to print the chess board
+void printBoard()
+{
+  int i, j;
+  printf("  ");
+  for (i = 0; i < N; i++)
+    printf("%d ", i);
+  printf("\n");
+  for (i = 0; i < N; i++)
+  {
+    printf("%d ", i);
+    for (j = 0; j < N; j++)
+      printf("%d ", board[i][j]);
+    printf("\n");
+  }
+}
+
+// Function to check if a queen can be placed in the given cell
+int isSafe(int row, int col)
+{
+  int i, j;
+  for (i = 0; i < col; i++) // Check row on left side
+    if (board[row][i])
+      return 0;
+  for (i = row, j = col; i >= 0 && j >= 0; i--, j--) // Check upper diagonal on left side
+    if (board[i][j])
+      return 0;
+  for (i = row, j = col; j >= 0 && i < N; i++, j--) // Check lower diagonal on left side
+    if (board[i][j])
+      return 0;
+  return 1;
+}
+
+// Recursive function to solve the N-Queens problem
+int solve(int col)
+{
+  int i;
+  if (col >= N) // Base case: All queens have been placed
+    return 1;
+  for (i = 0; i < N; i++)
+  {
+    if (isSafe(i, col))
+    {                     // Check if the queen can be placed in this cell
+      board[i][col] = 1;  // Place the queen in the cell
+      if (solve(col + 1)) // Recurse to place the next queen
+        return 1;
+      board[i][col] = 0; // Backtrack if a solution cannot be found
+    }
+  }
+  return 0;
+}
+
+int main()
+{
+  int i, j;
+  for (i = 0; i < N; i++)
+    for (j = 0; j < N; j++)
+      board[i][j] = 0;
+  if (solve(0))
+    printBoard();
+  else
+    printf("No solution found.");
+  return 0;
+}
