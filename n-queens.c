6d04208215b4c8a2afff30b1bde86d6f68d62013
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-04-12 01:34:09 +0530

    n-queens.c

---
+++ n-queens.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+#define N 8
+
+void print_board(int board[N][N])
+{
+  int i, j;
+  for (i = 0; i < N; i++)
+  {
+    for (j = 0; j < N; j++)
+    {
+      printf("%d ", board[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+int is_safe(int board[N][N], int row, int col)
+{
+  int i, j;
+
+  // Check this row on left side
+  for (i = 0; i < col; i++)
+  {
+    if (board[row][i])
+    {
+      return 0;
+    }
+  }
+
+  // Check upper diagonal on left side
+  for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+  {
+    if (board[i][j])
+    {
+      return 0;
+    }
+  }
+
+  // Check lower diagonal on left side
+  for (i = row, j = col; j >= 0 && i < N; i++, j--)
+  {
+    if (board[i][j])
+    {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int solve(int board[N][N], int col)
+{
+  int i;
+  if (col >= N)
+  {
+    return 1;
+  }
+
+  for (i = 0; i < N; i++)
+  {
+    if (is_safe(board, i, col))
+    {
+      board[i][col] = 1;
+
+      if (solve(board, col + 1))
+      {
+        return 1;
+      }
+
+      board[i][col] = 0;
+    }
+  }
+
+  return 0;
+}
+
+int main()
+{
+  int board[N][N] = {{0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0},
+                     {0, 0, 0, 0, 0, 0, 0, 0}};
+
+  if (solve(board, 0))
+  {
+    print_board(board);
+  }
+  else
+  {
+    printf("No solution found\n");
+  }
+
+  return 0;
+}
